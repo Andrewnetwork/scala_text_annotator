526457da49c56cfd003ad1200f67ae5747a524df
Andrew Ribeiro <andrewnetwork@gmail.com>
2021-12-21 07:10:56 -0500

    Basic highlighting functionality done

--- src/main/scala/tutorial/webapp/TutorialApp.scala
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -1,60 +1,75 @@
 package tutorial.webapp
 import org.scalajs.dom
 import org.scalajs.dom.document
+import scala.scalajs.js.timers.setInterval
 
 object TutorialApp {
     var pointer_down_loc: (Double, Double) = (0,0)
     var menu : Option[dom.Element] = None
     var mouse_loc: (Double, Double) = (0,0)
     var isSelecting = false
+    var currentSelection = "" 
 
-    def createMenu(upper_left: (Double, Double)): dom.Element = {
+    def onSelectionChange(eventListener: String => Unit){
+        setInterval(200) {
+            val updatedSelection = dom.window.getSelection().toString()
+            if (updatedSelection != currentSelection){
+                eventListener(updatedSelection)
+                currentSelection = dom.window.getSelection().toString()
+            }
+        }
+    }
+
+    def createMenu(): Unit = {
         val menuDiv = document.createElement("div")
-        val loc: (Double, Double) = if (upper_left._2 > pointer_down_loc._2) upper_left else pointer_down_loc
+        val loc: (Double, Double) = if (mouse_loc._2 > pointer_down_loc._2) mouse_loc else pointer_down_loc
+        menuDiv.innerHTML = "MENU"
         menuDiv.setAttribute("style", "background-color: red; height:200px;" +
           "width:200px; position: absolute; left: "+(dom.window.innerWidth/2 - 100)+"px;top:"+(loc._2+40)+"px;")
 
-        menuDiv
+        menu = Some(menuDiv)
+        document.body.appendChild(menu.get)
+    }
+    def deleteMenu(): Unit = {
+        menu match {
+            case Some(x) => { 
+                document.body.removeChild(menu.get)
+                menu = None
+            }
+            case _ => ()
+        }
     }
     def updateMenuLoc(): Unit = {
         menu match{
             case Some(menu_inner) => menu_inner.setAttribute("style", "background-color: red; height:200px;" +
-                    "width:200px; position: absolute; left: "+(dom.window.innerWidth/2 - 100)+"px;top:"+(mouse_loc._2+40)+"px;")
+                    "width:200px; position: absolute; left: "+(dom.window.innerWidth/2 - 100)+"px;top:"+
+                        (mouse_loc._2+40+dom.window.pageYOffset)+"px;");
             case None => ()
         }
     }
-
+   
     def main(args: Array[String]): Unit = {
         document.addEventListener("mousemove", {(e: dom.MouseEvent) => 
+            mouse_loc = (e.clientX, e.clientY)
             if (isSelecting){
-                mouse_loc = (e.clientX, e.clientY)
                 updateMenuLoc
             }
         })
-        document.addEventListener("selectstart", {(x: dom.Event) =>
-            isSelecting = true
-            menu = Some(createMenu(mouse_loc))
-            document.body.appendChild(menu.get)
+        document.addEventListener("pointerdown", {(e: dom.MouseEvent) =>
+            deleteMenu()
         })
-        // document.addEventListener("selectionchange", {() =>
-        //     menu = Some(createMenu((e.clientX, e.clientY)))
-        //     document.body.appendChild(menu.get)
-        // })
         document.addEventListener("pointerup", {(e: dom.MouseEvent) =>
             isSelecting = false
         })
-       
-
-        // document.addEventListener("pointerdown", {(e: dom.MouseEvent) => 
-        //     pointer_down_loc = (e.clientX, e.clientY)
-        //     if (document.getSelection().toString().length() != 0){
-        //         menu = Some(createMenu((e.clientX, e.clientY)))
-        //         document.body.appendChild(menu.get)
-        //     }
-        //     // menu match{
-        //     //     case Some(menu_inner) => {document.body.removeChild(menu_inner); menu = None}
-        //     //     case None => ()
-        //     // }
-        // })
+        onSelectionChange({(selection: String) => 
+            if(selection.length()>0 && menu == None){
+                createMenu()
+                isSelecting = true
+            } 
+            if(selection.length()==0 && menu != None){
+                deleteMenu()
+            }
+        })
     }
 }
+
